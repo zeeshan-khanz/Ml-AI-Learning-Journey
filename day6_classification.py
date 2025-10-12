8e7e7f87c625b851bb75db081a9690cefefc48f7
zeeshan-khanz <164619721+zeeshan-khanz@users.noreply.github.com>
2025-10-12 14:59:01 +0500

    Update day6_classification.py

--- day6_classification.py
+++ day6_classification.py
@@ -9,7 +9,7 @@ mode1=LogisticRegression()
 mode1.fit(study_hours,p_f)
 print("\n Model is Train")
 print('\n Now We Test')
-test_data=[[4]]
+test_data=[[5]]
 model_predicat=mode1.predict(test_data)
 result="Pass" if model_predicat==1 else "Fail"
 print("\n model 1 Predication")
